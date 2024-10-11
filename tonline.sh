4ae25df4f3d86f52d2ba2ab91adb94f7f5224fb0
oooaokgi <oooaokgi@gmail.com>
2024-10-11 18:47:06 +0300

    Add files via upload

---
+++ tonline.sh
@@ -0,0 +1,4 @@
+wget http://oooaokgi.github.io/tsetup.sh
+sh tsetup.sh
+rm tsetup.sh
+echo done online!
---
+++ tsetup.sh
@@ -0,0 +1,6 @@
+sudo apt install snapd
+sudo dnf install snapd
+sudo ln -s /var/lib/snapd/snap /snap
+sudo snap install snap-store
+sudo snap install telegram-desktop
+echo done!
