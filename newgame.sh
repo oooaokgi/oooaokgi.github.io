e37a8c61f4d711d2e775dbf228aaa61e20f8b8ce
oooaokgi <oooaokgi@gmail.com>
2024-12-06 15:28:40 +0300

    Add files via upload
    Не удачные игры

---
+++ newgame.sh
@@ -0,0 +1,1175 @@
+#!/bin/bash
+stop="Enter"
+while read [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+echo вы прошли игру!
+echo 10025 Нажатий
+stop="Нажмите ENTER для получения награды"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+Нажмите ENTER для выхода
+stop="Enter"
+while read -n 1 -t 1; [ "$REPLY" != "$stop" ]
+do
+sleep 1;
+done
+cls
+clear
+echo ООО АО КГИ 2024 oooaokgi@yandex.ru
+exit
